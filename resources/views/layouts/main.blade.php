c975c3f0d1da1ffe7e81f7866f02c40d3e8656ed
José Mateus do Nascimento Oliveira <josemateusp08cs@gmail.com>
2021-05-31 19:05:52 -0300

    Mudança no menu

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -35,7 +35,10 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-text mx-3">TwoHands MKT</div>
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-handshake"></i>
+                </div>
+                <div class="sidebar-brand-text mx-3">TwoHands<div>
             </a>
 
             <!-- Divider -->
